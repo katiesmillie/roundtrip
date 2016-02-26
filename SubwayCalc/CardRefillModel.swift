bf52ad20bb8ce91c8163bbc408c2853c96522d62
Katie Smillie <katiesmillie@gmail.com>
2016-02-26 11:24:59 -0500

    Use variable names for clarity

--- SubwayCalc/CardRefillModel.swift
+++ SubwayCalc/CardRefillModel.swift
@@ -10,12 +10,17 @@ import Foundation
 
 class CardRefillModel {
     
+    public let subwayFare = 2.75
+    public let bonusDivisor = 1.11
+    public let bonusMultiplier = 0.11
+    public let minFareForBonus = 5.50
+    public let centIncrement = 5.0
+    public let naivePercent = 100.0
+    
     let initialAmountOnCard: Double
     let numberOfRides: Int
-    let subwayFare: Double = 2.75
     
-    
-    init (initialAmountOnCard: Double, numberOfRides: Int){
+    init (initialAmountOnCard: Double, numberOfRides: Int) {
         self.initialAmountOnCard = initialAmountOnCard
         self.numberOfRides = numberOfRides
     }
@@ -28,13 +33,13 @@ class CardRefillModel {
         return self.finalCardValue() - initialAmountOnCard
     }
     
-    /** This is the amount needed divided 1.11 to account for the MTA bonus of 11% */
+    // This is the amount needed divided 1.11 to account for the MTA bonus of 11%
    func amountToPutInMachine() -> Double {
-        return self.roundTo100s(self.totalAmountNeeded() / 1.11)
+        return self.roundTo100s(self.totalAmountNeeded() / bonusDivisor)
     }
     
     func bonus() -> Double {
-        return self.roundTo100s(self.amountToPutInMachine() * 0.11)
+        return self.roundTo100s(self.amountToPutInMachine() * bonusMultiplier)
     }
     
     func isValid() -> Bool {
@@ -45,22 +50,20 @@ class CardRefillModel {
     }
     
     private func needsEnoughMoney() -> Bool {
-        return self.amountToPutInMachine() >= 5.5
+        return self.amountToPutInMachine() >= minFareForBonus
     }
     
     private func isDivisibleBy5Cents() -> Bool {
-        return (self.amountToPutInMachine() * 100) % 5 == 0
+        return (self.amountToPutInMachine() * naivePercent % centIncrement == 0)
     }
     
     private func isNotGreaterThanOneHundred() -> Bool {
-        return (self.finalCardValue() <= 100.0)
+        return (self.finalCardValue() <= naivePercent)
     }
     
     private func roundTo100s(number: Double) -> Double {
-        return round(100 * number) / 100
+        return round(naivePercent * number) / naivePercent
     }
-    
-    
     
     
 }
